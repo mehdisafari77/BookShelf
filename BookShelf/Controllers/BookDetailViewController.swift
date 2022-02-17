a7908daa36dc339fa413d9d2066006492fce66d1
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-17 11:26:24 -0800

    done with code, bug fixing

--- BookShelf/Controllers/BookDetailViewController.swift
+++ BookShelf/Controllers/BookDetailViewController.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class BookDetailViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(red: 49 / 255, green: 163 / 255, blue: 159 / 255, alpha: 1.0)
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateViews() {
+        guard let book = book, isViewLoaded else { return }
+        
+        bookTitleLabel.text = book.bookTitle
+        authorNameLabel.text = book.authorName
+        coverImgLabel.contentMode = .scaleAspectFit
+        coverImgLabel.image = UIImage(named: book.coverImg)
+        releaseYearLabel.text = "\(book.releaseYear)"
+        descriptionLabel.text = book.description
     }
-    */
-
+    
+    var book: Book? {
+        didSet{
+            updateViews()
+        }
+    }
+    
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    @IBOutlet weak var coverImgLabel: UIImageView!
+    @IBOutlet weak var releaseYearLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
 }
